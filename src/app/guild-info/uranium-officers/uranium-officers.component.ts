ce264fe9634614a68f660b33931709523bd183c6
Michael Ziluck <ziluckmichael@gmail.com>
2020-09-25 17:32:32 -0500

    Update to officer setup

--- src/app/guild-info/uranium-officers/uranium-officers.component.ts
+++ src/app/guild-info/uranium-officers/uranium-officers.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Globals } from 'app/globals';
+import { of } from 'rxjs';
+import { OfficerData, OFFICER_DATA } from './uranium-officers.data';
 
 @Component({
     selector: 'app-uranium-officers',
@@ -7,6 +9,8 @@ import { Globals } from 'app/globals';
     styleUrls: ['./uranium-officers.component.scss'],
 })
 export class UraniumOfficersComponent implements OnInit {
+    officers: OfficerData[] = OFFICER_DATA;
+
     constructor(
         private globals: Globals,
     ) {
@@ -14,5 +18,20 @@ export class UraniumOfficersComponent implements OnInit {
 
     ngOnInit(): void {
         this.globals.titleName = 'Your Officers';
+    }
+
+    resolveUrl(url: string): string {
+        if (url) {
+            return url;
+        }
+        return 'assets/officers/blank.jpg';
+    }
+
+    resolveDescription(officer: OfficerData): string {
+        if (officer?.description) {
+            return officer.description;
+        }
+        const pictureCheck: string = officer.imageUrl ? '' : 'or a picture ';
+        return `Hi! My name is ${officer.name} and I didn't provide a description ${pictureCheck}for the website.`;
     }
 }
---
+++ src/app/guild-info/uranium-officers/uranium-officers.data.ts
@@ -0,0 +1,72 @@
+export interface OfficerData {
+    title: string;
+    name: string;
+    imageUrl: string;
+    description: string;
+}
+
+export const OFFICER_DATA: OfficerData[] = [
+    {
+        title: 'Commanding Officer',
+        name: 'Darnell',
+        description: null,
+        imageUrl: null,
+    },
+    {
+        title: 'Executive Officer',
+        name: 'Astakyr',
+        description: null,
+        imageUrl: null,
+    },
+    {
+        title: 'Raid Leader][Gold Team',
+        name: 'Scoffsstab',
+        description: 'Hi, I’m Chris, purveyor of exquisite tattoos and fine roguishery. I’m in charge of making sure ' +
+            'we do the things to kill the big monsters. Also I’m a career bartender so I’m in charge of what happens ' +
+            'usually if we DON’T kill the big monsters.',
+        imageUrl: 'assets/officers/scoffsstab.jpg',
+    },
+    {
+        title: 'Raid Leader][Blue Team',
+        name: 'Luke / Felowynn',
+        description: 'Hey my name is Luke. I\'ve been long time friends with some of the guild leadership since I ' +
+            'was but a wee lad. I play second fiddle to our good man Scoff making sure that we still kill the ' +
+            'slightly less large and scary monsters. I enjoy seeing new faces and watching the guild grow as a ' +
+            'community. Personally, I rather enjoy traveling and nice hats.',
+        imageUrl: 'assets/officers/luke-felowynn.jpg',
+    },
+    {
+        title: 'Recruiting Officer',
+        name: 'Ben / Aelionys',
+        description: 'Ben here AKA Aelionys, been a part of the guild since the beginning of BFA. I help with finding' +
+            ' members that want to have fun and are willing to take on the challenges World of Warcraft has to offer.' +
+            ' Am always willing to help when I can and love seeing the guild grow. I look forward to smashing each ' +
+            'raid tier and the exciting times we will have with fellow guildies. If you have any question feel free ' +
+            'to reach out on Discord or in game. Favorite WoW quote, “Sometime the hand of fate must be forced.”',
+        imageUrl: 'assets/officers/ben-aelionys.jpg',
+    },
+    {
+        title: 'Media Officer',
+        name: 'Meeklae / Mike',
+        description: 'Despite having written the website, I haven\'t actually written a description for myself yet. Whoops!',
+        imageUrl: null,
+    },
+    {
+        title: 'DPS Lead',
+        name: 'McDingus',
+        description: 'Formed during the most bodacious metal riff known to man whilst lightning struck a flaming unicorn, McDingus was brought into this world to hack, slash, and otherwise kick ass. Bearing the mark of Rage, his temper can only be quelled by l33t d33ps and gummy bears. Armed with his Beard of Destiny, he seeks to triumph over AOTC and Cutting Edge at every turn, striking down trash and looking sick as hell doing it. With his trusty chicken steed "Thic Dingus", he charges into battle knowing only the badass smacks he reigns upon his enemies will bring about his salvation.',
+        imageUrl: 'assets/officers/mc-dingus.png',
+    },
+    {
+        title: 'Heal Lead',
+        name: 'Rinny',
+        description: null,
+        imageUrl: null,
+    },
+    // {
+    //     title: '',
+    //     name: '',
+    //     description: '',
+    //     imageUrl: '',
+    // },
+];
